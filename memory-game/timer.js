b40852ae22a9fd247c460de72489214adbd1db17
obragaa <f.braga.duarte02@gmail.com>
2020-07-05 22:04:24 -0300

    Finalização no 'timer.js'

--- memory-game/timer.js
+++ memory-game/timer.js
@@ -4,20 +4,17 @@ var hh = 0;
 var mm = 0;
 var ss = 0;
 
-var tempo = 1000;//Quantos milésimos valem 1 segundo?
+var time = 1000;
 var cron;
 
-//Inicia o temporizador
 function start() {
-    cron = setInterval(() => { timer(); }, tempo);
+    cron = setInterval(() => { timer(); }, time);
 }
 
-//Para o temporizador mas não limpa as variáveis
 function pause() {
     clearInterval(cron);
 }
 
-//Para o temporizador e limpa as variáveis
 function stop() {
     clearInterval(cron);
     hh = 0;
@@ -27,26 +24,22 @@ function stop() {
     document.getElementById('counter').innerText = '00:00:00';
 }
 
-//Faz a contagem do tempo e exibição
 function timer() {
-    ss++; //Incrementa +1 na variável ss
+    ss++;
 
-    if (ss == 59) { //Verifica se deu 59 segundos
-        ss = 0; //Volta os segundos para 0
-        mm++; //Adiciona +1 na variável mm
+    if (ss == 59) {
+        ss = 0;
+        mm++;
 
-        if (mm == 59) { //Verifica se deu 59 minutos
-            mm = 0;//Volta os minutos para 0
-            hh++;//Adiciona +1 na variável hora
+        if (mm == 59) {
+            mm = 0;
+            hh++;
         }
     }
-
-    //Cria uma variável com o valor tratado HH:MM:SS
+    
     var format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
     
-    //Insere o valor tratado no elemento counter
     document.getElementById('counter').innerText = format;
-
-    //Retorna o valor tratado
+    
     return format;
 }
